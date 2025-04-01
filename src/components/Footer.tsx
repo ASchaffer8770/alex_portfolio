08c31f341c75a8fddd5c84bc4b853f83fa534e4e
Aschaffer8770 <aschaffer8770@gmail.com>
2025-03-31 17:44:47 -0700

    final eslint fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <p>&copy; "2025 Alex's Portfolio. All rights reserved."</p>)
+            <p>&copy; {`2025 Alex's Portfolio. All rights reserved.`}</p>
             <nav>
                 <a href="#about">About</a>
                 <a href="#projects">Projects</a>
