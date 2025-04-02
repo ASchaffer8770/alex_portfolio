3fe5d7b23ffb8d8da2ac73d66893a97dca24ff71
Aschaffer8770 <aschaffer8770@gmail.com>
2025-04-02 11:03:51 -0700

    Vercel Speed Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
@@ -50,6 +51,7 @@ export default function Home() {
             View My Experience
           </a>
           <Analytics />
+          <SpeedInsights />
         </section>
 
         {/* About Section */}
